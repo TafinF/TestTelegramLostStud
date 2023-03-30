bf4ae74e3c4bffebcd754b5721a719fb7efd7dfc
TafinF <129420385+TafinF@users.noreply.github.com>
2023-03-30 23:24:25 +0300

    Add files via upload

--- scrint.js
+++ scrint.js
@@ -11,7 +11,7 @@ for (var i = 1; i < dataArr.length; i = i + 1) {
 }
 CLASS_NAME = dataArr[0].split("=")[1]
 console.log(STUD_ARR)
-window.Telegram.WebApp.MainButton.isVisible = true
+
 function decode(str) {
     let recipeMap = new Map([
         ['10', 'а'], ['11', 'б'], ['12', 'в'], ['13', 'г'], ['14', 'д'], ['15', 'е'], ['16', 'ё'], ['17', 'ж'], ['18', 'з'], ['19', 'и'], ['20', 'й'], ['21', 'к'], ['22', 'л'], ['23', 'м'], ['24', 'н'], ['25', 'о'], ['26', 'п'], ['27', 'р'], ['28', 'с'], ['29', 'т'], ['30', 'у'], ['31', 'ф'], ['32', 'х'], ['33', 'ц'], ['34', 'ч'], ['35', 'ш'], ['36', 'щ'], ['37', 'ъ'], ['38', 'ы'], ['39', 'ь'], ['40', 'э'], ['41', 'ю'], ['42', 'я']
@@ -89,3 +89,15 @@ window.onload = function () {
     }
     console.log(STUD_ARR)
 };
+
+
+let tg = window.Telegram.WebApp; //получаем объект webapp телеграма 
+
+tg.expand(); //расширяем на все окно  
+
+tg.MainButton.text = "Отправить сведения"; //изменяем текст кнопки 
+tg.MainButton.isVisible = true
+Telegram.WebApp.onEvent('mainButtonClicked', function () {
+    tg.sendData(GetCallback());
+    //при клике на основную кнопку отправляем данные в строковом виде
+});
